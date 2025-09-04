4c26ebb5e152d0d36c5503b85bd669652905fffb
mohammadbrkkk <163210962+mohammadbrkkk@users.noreply.github.com>
2025-09-05 00:44:12 +0330

    update_test_to_project

--- src/components/ind.jsx
+++ src/components/ind.jsx
@@ -1,40 +1,40 @@
 // create card and show in UI
-const CreateCard = () => {
-  // const {
-  //   name = "none",
-  //   lastName = "none",
-  //   Email = "none",
-  //   Title = "none",
-  //   company = "none",
-  //   companySize = "none",
-  //   Comment = "none",
-  // } = card;
+const CreateCard = ({ card, key }) => {
+  console.log(key);
+
+  const {
+    name = "none",
+    lastName = "none",
+    Email = "none",
+    Title = "none",
+    company = "none",
+    companySize = "none",
+    Comment = "none",
+  } = card;
 
   return (
     <>
-      <div className=" flex m-5 w-2/5 h-80  bg-amber-50 rounded-md shadow-2xl ">
+      <div className=" flex m-5 w-2/5 h-80  bg-white/30  rounded-md shadow-2xl ">
         <div className="flex flex-col items-center w-full mt-2.5">
-          <h1 className="t font-extrabold">john Doe </h1>
-          <p className="">mohammadbrk4444@gmail.com</p>
+          <h1 className="t font-extrabold">
+            {name} {lastName}
+          </h1>
+          <p className="">{Email}</p>
           <div className="mt-5">
             <div className="mx-2 mb-2">
               <span className=" font-extrabold">Job Title: </span>
-              <span>senior produact manager</span>
+              <span>{Title}</span>
             </div>
             <div className="mx-2 mb-2">
               <span className="font-extrabold">Company : </span>
-              <span>dasdas daskodjasosa </span>
+              <span>{company}</span>
             </div>
             <div className="mx-2 mb-2">
-              <span className="font-extrabold">Company : </span>
-              <span>1/3 employes</span>
+              <span className="font-extrabold">Company Size: </span>
+              <span>{companySize} employes</span>
             </div>
             <div className="mt-10 mx-2">
-              <p>
-                {" "}
-                " Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Error, neque beatae rem rerum harum deleniti. ""
-              </p>
+              <p>{Comment}</p>
             </div>
           </div>
         </div>
